227a30bc5217295211d693c3791c70b325c05414
Ga1kinSasha <115646309+Ga1kinSasha@users.noreply.github.com>
2024-11-09 18:07:04 +0500

    5.2

--- src/components/review/review.tsx
+++ src/components/review/review.tsx
@@ -1,47 +1,42 @@
-import React from 'react';
+import { ReviewType } from "../../mocks/reviews";
 
-export function ReviewForm(): JSX.Element {
-    const [formData, setFormData] = React.useState({
-        review: '',
-        rating: 1
-    });
-
-    const handleFieldChange = (evt: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const {name, value} = evt.target;
-        setFormData({...formData, [name]: value});
-    };
-
-  
-    const ratingStars = (value: number, title: string) => (
-        <>
-            <input className="form__rating-input visually-hidden" onChange={handleFieldChange} name="rating" value={value} id={`${value}-stars`} type="radio" />
-            <label htmlFor={`${value}-stars`} className="reviews__rating-label form__rating-label" title={title}>
-                <svg className="form__star-image" width="37" height="33">
-                    <use xlinkHref="#icon-star"></use>
-                </svg>
-            </label>
-        </>
-    );
-    
-
-    return (
-        <form className="reviews__form form" action="#" method="post">
-            <label className="reviews__label form__label" htmlFor="review">Your review</label>
-            <div className="reviews__rating-form form__rating">
-                {ratingStars(5, 'perfect')}
-                {ratingStars(4, 'good')}
-                {ratingStars(3, 'not bad')}
-                {ratingStars(2, 'badly')}
-                {ratingStars(1, 'terribly')}
-            </div>
-            <textarea className="reviews__textarea form__textarea" onChange={handleFieldChange} value={formData.review} id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"/>
-            <div className="reviews__button-wrapper">
-                <p className="reviews__help">
-                    To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-                </p>
-                <button className="reviews__submit form__submit button" type="submit" disabled>Submit</button>
-            </div>
-        </form>
-    );
+interface SingleReviewProps {
+  review: ReviewType;
 }
 
+export const SingleReview: React.FC<SingleReviewProps> = ({ review }) => {
+  const reviewDate = new Date(review.date);
+  const formattedDate = reviewDate.toLocaleString('en', { month: 'long', year: 'numeric' });
+  
+  return (
+    <div className="reviews__user user">
+      <div className="reviews__avatar-wrapper user__avatar-wrapper">
+        <img
+          className="reviews__avatar user__avatar"
+          src={review.user.avatarUrl}
+          width={54}
+          height={54}
+          alt="User avatar"
+        />
+      </div>
+      <span className="reviews__user-name">{review.user.name || 'Anonymous'}</span>
+
+      <div className="reviews__info">
+        <div className="reviews__rating rating">
+          <div className="reviews__stars rating__stars">
+            <span style={{ width: `${review.rating * 20}%` }} />
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+
+        <p className="reviews__text">{review.comment}</p>
+
+        <time className="reviews__time" dateTime={reviewDate.toISOString()}>
+          {formattedDate}
+        </time>
+      </div>
+    </div>
+  );
+};
+
+export default SingleReview;
--- src/pages/offer/offer-page.tsx
+++ src/pages/offer/offer-page.tsx
@@ -1,187 +1,175 @@
 import { fullOffers } from "../../mocks/in-depth-offer";
 import { useParams } from "react-router-dom";
 import { reviews } from "../../mocks/reviews";
-import { ReviewForm } from "../../components/review/review";
-import { OfferList } from "../../components/list-of-offers/list-of-offers";
-import { offers } from "../../mocks/offer";
+import { ReviewForm } from "../../components/review-form/review-form";
+import { ReviewList } from "../../components/list-of-review/list-of-review";
+import Map from "../../components/map/map";
+import { MapType } from "../../exports/map-types";
+import NearbyOffersList from "../../components/adjacent-offers/adjacent-offers";
+import { nearbyOffers } from "../../mocks/adjacent-offers";
 
-export function Offer(){
-    const {id} = useParams<{ id: string }>();
-    let offer = fullOffers.find((offer) => offer.id === id)
-    let offerReviews = reviews.filter((offer) => offer.id === id)
-    if (!offer) {
-      return <div>Offer not found</div>;
-    }
-    return (
-      <div className="page">
-        <header className="header">
-          <div className="container">
-            <div className="header__wrapper">
-              <div className="header__left">
-                <a className="header__logo-link" href="main.html">
-                  <img
-                    className="header__logo"
-                    src="img/logo.svg"
-                    alt="6 cities logo"
-                    width={81}
-                    height={41}
-                  />
-                </a>
-              </div>
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  <li className="header__nav-item user">
-                    <a
-                      className="header__nav-link header__nav-link--profile"
-                      href="#"
-                    >
-                      <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                      <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                      </span>
-                      <span className="header__favorite-count">3</span>
-                    </a>
-                  </li>
-                  <li className="header__nav-item">
-                    <a className="header__nav-link" href="#">
-                      <span className="header__signout">Sign out</span>
-                    </a>
-                  </li>
-                </ul>
-              </nav>
+export function Offer() {
+  const { id } = useParams<{ id: string }>();
+  const currentOffer = fullOffers.find((offer) => offer.id === id);
+  const offerReviews = reviews.filter((offer) => offer.id === id);
+  const currentOfferNearbies = nearbyOffers.find((offer) => offer.id === id);
+
+  if (!currentOffer) {
+    return <div>Offer not found</div>;
+  }
+
+  const {
+    images,
+    isPremium,
+    title,
+    rating,
+    type,
+    bedrooms,
+    maxAdults,
+    price,
+    goods,
+    host,
+    description,
+    city
+  } = currentOffer;
+
+  return (
+    <div className="page">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link" href="main.html">
+                <img
+                  className="header__logo"
+                  src="img/logo.svg"
+                  alt="6 cities logo"
+                  width={81}
+                  height={41}
+                />
+              </a>
             </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="#">
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    <span className="header__favorite-count">3</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="#">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </header>
-        <main className="page__main page__main--offer">
-          <section className="offer">
-            <div className="offer__gallery-container container">
-              <div className="offer__gallery">
-              {offer.images.map((image) => (
+        </div>
+      </header>
+
+      <main className="page__main page__main--offer">
+        <section className="offer">
+          <div className="offer__gallery-container container">
+            <div className="offer__gallery">
+              {images.map((image) => (
                 <div key={image} className="offer__image-wrapper">
                   <img className="offer__image" src={image} alt="Photo studio" />
                 </div>
               ))}
-              </div>
             </div>
-            <div className="offer__container container">
-              <div className="offer__wrapper">
-                {offer.isPremium &&
+          </div>
+
+          <div className="offer__container container">
+            <div className="offer__wrapper">
+              {isPremium && (
                 <div className="offer__mark">
                   <span>Premium</span>
                 </div>
-                }
-                <div className="offer__name-wrapper">
-                  <h1 className="offer__name">
-                      {offer.title}
-                  </h1>
-                  <button className="offer__bookmark-button button" type="button">
-                    <svg className="offer__bookmark-icon" width={31} height={33}>
-                      <use xlinkHref="#icon-bookmark"/>
-                    </svg>
-                    <span className="visually-hidden">To bookmarks</span>
-                  </button>
-                </div>
-                <div className="offer__rating rating">
-                  <div className="offer__stars rating__stars">
-                    <span style={{ width: `${(offer.rating / 5) * 100}%` }}/>
-                    <span className="visually-hidden">Rating</span>
-                  </div>
-                  <span className="offer__rating-value rating__value">{offer.rating}</span>
-                </div>
-                <ul className="offer__features">
-                  <li className="offer__feature offer__feature--entire">
-                      {offer.type}
-                  </li>
-                  <li className="offer__feature offer__feature--bedrooms">
-                      {offer.bedrooms} Bedrooms
-                  </li>
-                  <li className="offer__feature offer__feature--adults">
-                      Max {offer.maxAdults} adults
-                  </li>
-                </ul>
-                <div className="offer__price">
-                  <b className="offer__price-value">€{offer.price}</b>
-                  <span className="offer__price-text">&nbsp;night</span>
-                </div>
-                <div className="offer__inside">
-                  <h2 className="offer__inside-title">Whats inside</h2>
-                  <ul className="offer__inside-list">
-                    {
-                      offer.goods.map((item) => ( <li className="offer__inside-item">{item}</li>))
-                    }
-                  </ul>
-                </div>
-                <div className="offer__host">
-                  <h2 className="offer__host-title">Meet the host</h2>
-                  <div className="offer__host-user user">
-                    <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
-                      <img
-                        className="offer__avatar user__avatar"
-                        src={offer.host.avatarUrl}
-                        width={74}
-                        height={74}
-                        alt="Host avatar"
-                      />
-                    </div>
-                    <span className="offer__user-name">{offer.host.name}</span>
-                    {offer.host.isPro && <span className="offer__user-status">Pro</span>}
-                  </div>
-                  <div className="offer__description">
-                    <p className="offer__text">
-                        {offer.description}
-                    </p>
-                  </div>
-                </div>
-                <section className="offer__reviews reviews">
-                  <h2 className="reviews__title">
-                      Reviews · <span className="reviews__amount">{offerReviews.length}</span>
-                  </h2>
-                  {offerReviews.map((review) => (
-                  <ul className="reviews__list">
-                    <li className="reviews__item">
-                      <div className="reviews__user user">
-                        <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                          <img
-                            className="reviews__avatar user__avatar"
-                            src={review.user.avatarUrl}
-                            width={54}
-                            height={54}
-                            alt="Reviews avatar"
-                          />
-                        </div>
-                        <span className="reviews__user-name">{review.user.name}</span>
-                      </div>
-                      <div className="reviews__info">
-                        <div className="reviews__rating rating">
-                          <div className="reviews__stars rating__stars">
-                            <span style={{width: `${review.rating * 20}%`}}/>
-                            <span className="visually-hidden">Rating</span>
-                          </div>
-                        </div>
-                        <p className="reviews__text">
-                            {review.comment}
-                        </p>
-                        <time className="reviews__time" dateTime={new Date(review.date).toISOString()}>
-                          {new Date(review.date).toLocaleString('EN', { month: 'long', year: 'numeric' })}
-                        </time>
-                      </div>
-                    </li>
-                  </ul>
-                  ))}
-                  <ReviewForm></ReviewForm>
-                </section>
+              )}
+
+              <div className="offer__name-wrapper">
+                <h1 className="offer__name">{title}</h1>
+                <button className="offer__bookmark-button button" type="button">
+                  <svg className="offer__bookmark-icon" width={31} height={33}>
+                    <use xlinkHref="#icon-bookmark" />
+                  </svg>
+                  <span className="visually-hidden">To bookmarks</span>
+                </button>
               </div>
+
+              <div className="offer__rating rating">
+                <div className="offer__stars rating__stars">
+                  <span style={{ width: `${(rating / 5) * 100}%` }} />
+                  <span className="visually-hidden">Rating</span>
+                </div>
+                <span className="offer__rating-value rating__value">{rating}</span>
+              </div>
+
+              <ul className="offer__features">
+                <li className="offer__feature offer__feature--entire">{type}</li>
+                <li className="offer__feature offer__feature--bedrooms">{bedrooms} Bedrooms</li>
+                <li className="offer__feature offer__feature--adults">Max {maxAdults} adults</li>
+              </ul>
+
+              <div className="offer__price">
+                <b className="offer__price-value">€{price}</b>
+                <span className="offer__price-text">&nbsp;night</span>
+              </div>
+
+              <div className="offer__inside">
+                <h2 className="offer__inside-title">Whats inside</h2>
+                <ul className="offer__inside-list">
+                  {goods.map((item) => (
+                    <li key={item} className="offer__inside-item">
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              <div className="offer__host">
+                <h2 className="offer__host-title">Meet the host</h2>
+                <div className="offer__host-user user">
+                  <div className="offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper">
+                    <img
+                      className="offer__avatar user__avatar"
+                      src={host.avatarUrl}
+                      width={74}
+                      height={74}
+                      alt="Host avatar"
+                    />
+                  </div>
+                  <span className="offer__user-name">{host.name}</span>
+                  {host.isPro && <span className="offer__user-status">Pro</span>}
+                </div>
+                <div className="offer__description">
+                  <p className="offer__text">{description}</p>
+                </div>
+              </div>
+
+              <section className="offer__reviews reviews">
+                <h2 className="reviews__title">
+                  Reviews · <span className="reviews__amount">{offerReviews.length}</span>
+                </h2>
+                <ReviewList offerReviews={offerReviews} />
+                <ReviewForm />
+              </section>
             </div>
-            <section className="offer__map map"/>
-          </section>
-          <div className="container">
-            <section className="near-places places">
-              <h2 className="near-places__title">
-                  Other places in the neighbourhood
-              </h2>
-              <OfferList offers={offers.filter((offer) => offer.id !== id)} />
-            </section>
           </div>
-        </main>
-      </div>);
-  }
+
+          <Map city={city} offers={currentOfferNearbies?.nearbyOffersIds || []} mapType={MapType.offerMap} />
+        </section>
+
+        {currentOfferNearbies ? (
+          <NearbyOffersList offers={currentOfferNearbies.nearbyOffersIds} />
+        ) : (
+          <section className="near-places places">
+            <h2 className="near-places__title">No offers nearby</h2>
+          </section>
+        )}
+      </main>
+    </div>
+  );
+}
